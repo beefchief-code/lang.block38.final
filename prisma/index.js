0cbe717f3227155825c1b15c0bd2d1031617c582
beefchief <c.lang.1993@gmail.com>
2024-10-16 23:14:34 -0500

    finish index

--- prisma/index.js
+++ prisma/index.js
@@ -1,24 +1,27 @@
-const { PrismaClient } = require("@prisma/client");
 const bcrypt = require("bcrypt");
+
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient().$extends({
   model: {
     user: {
-      async register(username, password) {
-        const hash = await bcrypt.hash(password, 10);
+      async register(email, password) {
+        const saltRounds = 12;
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
         const user = await prisma.user.create({
-          data: { username, password: hash },
+          data: { email, password: hashedPassword },
         });
         return user;
       },
-      async login(username, password) {
+      async login(email, password) {
         const user = await prisma.user.findUniqueOrThrow({
-          where: { username },
+          where: { email },
         });
-        const valid = await bcrypt.compare(password, user.password);
-        if (!valid) throw Error("Invalid password");
+        const calid = await bcrypt.compare(password, user.password);
+        if (!valid) throw Error("Password Invalid");
         return user;
       },
     },
   },
 });
+
 module.exports = prisma;
